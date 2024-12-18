90e43709658d5a306990164da8d805bad5770b9a
Yair Yarom <irush@cs.huji.ac.il>
2024-12-18 13:50:25 +0200

    healthcheck.d: needs-reboot: reboot on completing for too long

--- healthcheck.d/needs-reboot.sh
+++ healthcheck.d/needs-reboot.sh
@@ -18,7 +18,24 @@ case "${state}" in
             boottime=`awk '$1=="btime"{print $2}' /proc/stat`
 
             # if idle or down, check if before or after reboot
-            if [[ "$state" = "IDLE+DRAIN" || "$state" = "RESERVED+DRAIN" || "$state" = "MAINT+DRAIN" || "$state" = "DOWN*+DRAIN" ]]; then
+            if [[ "$state" = "IDLE+DRAIN" \
+                      || "$state" = "RESERVED+DRAIN" \
+                      || "$state" = "MAINT+DRAIN" \
+                      || "$state" = "DOWN*+DRAIN" \
+                      || "$state" = "IDLE+COMPLETING+DRAIN" \
+                      || "$state" = "IDLE*+COMPLETING+DRAIN" \
+                ]]; then
+                # special case when stuck on completing, check for how long
+                # it's like this, more than an hour - reboot
+                if [[ "$state" = "IDLE+COMPLETING+DRAIN" || "$state" = "IDLE*+COMPLETING+DRAIN" ]]; then
+                    endtime=`squeue -w "${node}" -h -Oendtime | sort | tail -n 1`
+                    endtime=`date -d $endtime +%s`
+                    lastcomp=$((`date +%s` - endtime))
+                    if [[ $lastcomp -lt $((60 * 60)) ]]; then
+                        echo 'needs reboot'
+                        exit 1;
+                    fi
+                fi
                 if [[ $draintime -lt $boottime ]]; then
                     # drain before reboot, can resume
                     exit 0
